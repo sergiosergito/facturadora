61f91deb14393ba4e1f952c9336572be610fbb5c
serginho6x <serginho_6x@hotmail.com>
2020-06-08 21:18:37 -0400

    reads Json

--- src/main/java/com/kindsonthegenius/demo/HomeController.java
+++ src/main/java/com/kindsonthegenius/demo/HomeController.java
@@ -12,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class HomeController {
 
+	private final static String URL = "http://localhost:8080/tarifas";
+	
 	@RequestMapping(value = "/")
 	public static String Welcome() {
 		return "Welcome to Spring Boot";
@@ -20,10 +22,10 @@ public class HomeController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@GetMapping("/countries")
+	@GetMapping("/get/tarifas")
 	public List<Object> getCountries(){
-		String url = "https://restcountries.eu/rest/v2/name/australia";
-		Object[] objects = restTemplate.getForObject(url,Object[].class);
+		
+		Object[] objects = restTemplate.getForObject(URL,Object[].class);
 		return Arrays.asList(objects);
 	}
 }
